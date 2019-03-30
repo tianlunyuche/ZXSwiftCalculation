a91eb7955f1052227d7547f7c0f8b1342c691126
240394381@qq.com <240394381@qq.com>
2019-03-30 21:39:18 +0800

    栈，队列，搜索，排序算法

--- ZXSwiftCalculation/main.swift
+++ ZXSwiftCalculation/main.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import NotificationCenter
 print("Hello, World!")
 
 func add(_ num: Int) -> (Int) -> Int {
@@ -190,7 +191,195 @@ func quicksort(_ array: [Int]) -> [Int] {
     return quicksort(left) + middle + quicksort(right)
 }
 
-print(quicksort([2,3,1,323,2]))
+var array = [2,3,1,323,2]
+print(quicksort(array))
+//sorted 函数采用的是内省算法，由堆排序，插入排序，快排3种算法构成，依据输入的深度选择最佳算法来完成
+var newarray = array.sorted()
+
+//二分搜索，nums已经排好序了
+func binarySearch(_ nums: [Int], _ target: Int) -> Bool {
+    return binarySearch(nums: nums, target: target , left: 0, right: nums.count - 1)
+}
+
+func binarySearch(nums: [Int], target: Int, left: Int, right: Int) -> Bool {
+    guard left <= right else {
+        return false
+    }
+    let mid = (right - left) / 2 + left
+    if nums[mid] == target {
+        return true
+    } else if nums[mid] < target {
+        return binarySearch(nums: nums, target: target, left: mid + 1, right: right)
+    } else {
+        return binarySearch(nums: nums, target: target, left: left, right: mid - 1)
+    }
+}
+
+print(binarySearch(array, 323))
+
+//已知有很多会议，这些会议时间有重叠，就将它们合并
+//例如，一个会议时间 下午3点到5点， 另一个会议为下午4点到6点，那合并后的会议时间为下午3点到6点
+//已知有开始时间和结束时间，需要先对会议数组 比较 开始时间排序，如果开始时间相同，就比较结束时间；
+//排好序后，将第一个元素放入结果数组中，
+//然后遍历 比较，如果结果数组中最后元素的会议开始时间 大于 排序后的数组中当前的会议结束时间，结果数组就添加当前元素；
+//否则，就将结果数组的当前元素结束时间改为这两个元素的最大结束时间。
+public class MeetingTime {
+    public var start: Int
+    public var end: Int
+    public init(_ start: Int, _ end: Int) {
+        self.start = start
+        self.end = end
+    }
+}
+
+func merge(meetingTimes: [MeetingTime]) -> [MeetingTime] {
+    guard meetingTimes.count > 1 else {
+        return meetingTimes
+    }
+    //排序
+    var meetingTimes = meetingTimes.sorted {
+        if $0.start != $1.start {
+            return $0.start < $1.start
+        } else {
+            return $0.end < $1.end
+        }
+    }
+    
+    //新建结果数组
+    var res = [MeetingTime]()
+    res.append(meetingTimes[0])
+    
+    //遍历排序后的原数组，与结果数组归并
+    for i in 1 ..< meetingTimes.count {
+        let last = res[res.count - 1]
+        let current = meetingTimes[i]
+        if current.start > last.end {
+            res.append(current)
+        } else {
+            last.end = max(last.end, current.end)
+        }
+    }
+    return res
+}
+
+//深度优先搜索DFS ，和广度优先搜索BFS：二叉树的前序遍历和层级遍历的本质分别就是这两者
+//DFS的实现使用递归，而BFS的实现是利用队列和循环
+
+//动态规划—— 斐波拉契数列问题：
+func Fib(_ max: Int) -> Int {
+    var (prev, curr) = (0, 1)
+    
+    for _ in 1 ..< max {
+        (curr, prev) = (curr + prev, curr)
+    }
+    return curr
+}
+
+var num = Fib(50)
+
+//struct Beer : Codable {
+//    enum CodingKeys : String, CodingKey {
+//        case name
+//        case abc = "alcohol_by_volumn"
+//        case style
+//    }
+//}
+var num2 = 0
+DispatchQueue.global().async {
+    
+    for _ in 1...1000 {
+        num2 += 1
+    }
+    
+}
+for _ in 1...1000 {
+    num2 += 1
+}
+//
+var highPriorityQueue = DispatchQueue.global(qos: .userInitiated)
+
+var lowPriorityQueue = DispatchQueue.global(qos: .utility)
+
+let semaphore = DispatchSemaphore(value: 1)
+
+lowPriorityQueue.async {
+    semaphore.wait()
+    
+    for i in 0...10 {
+        
+        print(i)
+        
+    }
+    
+    semaphore.signal()
+    
+}
+
+highPriorityQueue.async {
+    
+    semaphore.wait()
+    
+    for i in 11...20 {
+        
+        print(i)
+        
+    }
+    semaphore.signal()
+}
+
+@objcMembers class User: NSObject {
+    dynamic var email: String
+    init(email: String) {
+        self.email = email
+    }
+}
+let user = User(email: "com")
+//注册观察者，回调中处理收到的更改通知
+let observation = user.observe(\.email) { (user, change) in
+    print("\(user.email)")
+}
+
+//更改主体对象的属性值，触发发送更改的通知
+user.email = "com2"
+
+//swift 使用POP 实现二分搜索查找
+extension Array where Element: Comparable {
+    public var isSorted: Bool {
+        var preIdx = startIndex
+        var currentIdx = startIndex + 1
+        
+        while currentIdx != endIndex {
+            if self[preIdx] > self[currentIdx] {
+                return false
+            }
+            
+            preIdx = currentIdx
+            currentIdx += 1
+        }
+        return true
+    }
+}
+
+func binarySearch<T: Comparable>(sortedElements: [T], for element: T) -> Bool {
+    //
+    assert(sortedElements.isSorted)
+    
+    var lo = 0, hi = sortedElements.count - 1
+    while lo <= hi {
+        let mid = lo + (hi - lo) / 2
+        if sortedElements[mid] == element {
+            return true
+        } else if sortedElements[mid] < element {
+            lo = mid + 1
+        } else {
+            hi = mid - 1
+        }
+    }
+    return false
+}
+
+
+print()
 
 
 
